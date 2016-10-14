71042cf5d7eca02cf73b31d2fe0b9ed06a51993d
L Q <l q>
2016-10-14 23:08:42 +0800

    springboot helloworld demo

---
+++ src/main/java/com/springboot/demo/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.springboot.demo;
+
+/**
+ * Created by qlxrc on 2016/10/14.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class HelloWorld {
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(HelloWorld.class, args);
+    }
+}
