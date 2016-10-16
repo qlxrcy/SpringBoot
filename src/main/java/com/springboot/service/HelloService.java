7294f697187775f17fa56ee040750c5b218af775
L Q <l q>
2016-10-16 15:12:35 +0800

    springboot  Junit单元测试

---
+++ src/main/java/com/springboot/service/HelloService.java
@@ -0,0 +1,14 @@
+package com.springboot.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by qlxrc on 2016/10/16.
+ */
+@Service
+public class HelloService {
+
+    public String getName(){
+        return "hello";
+    }
+}
