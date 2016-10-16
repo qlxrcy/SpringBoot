8ae007292987379c1a9a4697ff311a197820c4c5
L Q <l q>
2016-10-16 15:51:20 +0800

    Spring boot 文件上传（多文件上传）

--- src/main/java/com/springboot/demo/Application.java
+++ src/main/java/com/springboot/demo/Application.java
@@ -5,11 +5,15 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import javax.servlet.MultipartConfigElement;
 
 /**
  * Created by qlxrc on 2016/10/15.
@@ -27,6 +31,18 @@ public class Application {
     /*@Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
            return application.sources(Application.class);
+    }*/
+
+    /*@Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //// 设置文件大小限制 ,超了，页面会抛出异常信息，这时候就需要进行异常信息的处理了;
+        factory.setMaxFileSize("128KB"); //KB,MB
+        /// 设置总上传数据总大小
+        factory.setMaxRequestSize("256KB");
+        //Sets the directory location wherefiles will be stored.
+        //factory.setLocation("路径地址");
+        return factory.createMultipartConfig();
     }*/
 
     public static void main(String[] args) {
